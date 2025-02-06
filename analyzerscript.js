2d560bef455a6c031bddde1fd97404c9cd618c7b
Christian Lewis <Christianelewis64@gmail.com>
2025-02-06 15:07:53 -0500

    test

--- analyzerscript.js
+++ analyzerscript.js
@@ -1,16 +1,16 @@
 const viewtoggle = document.getElementById("viewtoggle");
-        const stylesheet = document.getElementById("stylesheet");
-        const advancedgraphics = document.getElementById("advancedgraphics");
-        viewtoggle.addEventListener("click", function() {
-            if (stylesheet.getAttribute("href") === "analyzerstyles.css") {
-                stylesheet.setAttribute("href", "simple.css");
-                advancedgraphics.remove()
-                const data = localStorage.getItem("processedData");
-                document.getElementById("fileContent").textContent = data;
+const stylesheet = document.getElementById("stylesheet");
+const advancedgraphics = document.getElementById("advancedgraphics");
+viewtoggle.addEventListener("click", function() {
+    if (stylesheet.getAttribute("href") === "analyzerstyles.css") {
+        stylesheet.setAttribute("href", "simple.css");
+        advancedgraphics.remove()
+        const data = localStorage.getItem("processedData");
+        document.getElementById("fileContent").textContent = data;
 
-            } else {
-                stylesheet.setAttribute("href", "analyzerstyles.css");
-                fileContent.textContent = '';
-                document.body.appendChild(advancedgraphics);
-            }
-        });
+    } else {
+        stylesheet.setAttribute("href", "analyzerstyles.css");
+        fileContent.textContent = '';
+        document.body.appendChild(advancedgraphics);
+    }
+});
