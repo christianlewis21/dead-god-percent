b1376bb81d700179ee6c7063bbff4a012b1b86b5
Christian Lewis <Christianelewis64@gmail.com>
2025-02-05 22:25:06 -0500

    simple view and advanced views (advanced will be incomplete for a bit)

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const fileInput = document.getElementById('myFile');
+    let positionTextMap = {};
 
-    function readHexPosition(dataView, position) {
-        if (position < 0 || position >= dataView.byteLength) {
-            throw new Error("Position out of bounds");
-        }
-        const value = dataView.getUint8(position);
-        return value === 0x01 ? true : value === 0x00 ? false : null;
-    }
-
+    fetch('achievements.json')
+        .then(response => response.json())
+        .then(data => {
+            positionTextMap = data;
+        });
+    
     fileInput.addEventListener('change', function(event) {
         const file = event.target.files[0];
         if (file && file.name.endsWith('.dat')) {
@@ -20,11 +19,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 for (let position = 0x21; position <= 0x2A0; position++) {
                     const booleanValue = readHexPosition(dataView, position);
                     if (!booleanValue) {
-                        output += `${position - 32}\n`;
+                        const achievementPosition = position-32;
+                        const text = positionTextMap[achievementPosition];
+                        output += `${text}\n`;
                     }
                 }
                 localStorage.setItem("processedData", output);
-                console.log("Data stored in localStorage");
                 window.location.href = 'analyzer.html';
             };
             reader.readAsArrayBuffer(file);
@@ -32,4 +32,12 @@ document.addEventListener("DOMContentLoaded", function() {
             alert('File is not a valid save file. A valid save file can be found at C:\\Program Files(x86)\\Steam\\userdata\\[SteamID]\\250900\\remote\\rep+persistentgamedata[1,2,3].dat');
         }
     });
+
+    function readHexPosition(dataView, position) {
+        if (position < 0 || position >= dataView.byteLength) {
+            throw new Error("Position out of bounds");
+        }
+        const value = dataView.getUint8(position);
+        return value === 0x01 ? true : value === 0x00 ? false : null;
+    }
 });
